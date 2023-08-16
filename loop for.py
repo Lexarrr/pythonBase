31ce8f2a727bba9c91437122c86be68d7e48438c
Lexarrr <etc.21@yandex.ru>
2023-08-16 21:04:29 +0300

    loop for range

--- loop for.py
+++ loop for.py
@@ -9,3 +9,26 @@ for item in a[1:len(a):2]:
 
 for index in range(1, len(a), 2):
     print(a[index])
+
+a1 = []
+
+# a.append(что добавить(значение)
+
+count = 0
+
+for x in reversed(range(11)):
+    a1.append(x)
+print(a1)
+
+a_b = ['first', 'second']
+members = 6
+
+for x in range(members):
+    print(a_b[x % len(a_b)])
+print('fs')
+
+aa = []
+
+for x in range(11):
+    aa.append(x)
+print(aa[0:len(a1):2])
