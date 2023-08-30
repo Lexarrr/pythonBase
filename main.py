c02f418ed0ee69cf35e6361993e965d03047f99d
Lexarrr <etc.21@yandex.ru>
2023-08-30 18:39:56 +0300

    main les6

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
-# списки
-
-array = []  # void array
-array2 = list()  # void array from func list
-array3 = [1, 2, 3, 4, 5, 6]
-list_4 = [36.6, 50, 'Temperature', False]
-array5 = list('Python')  # void array from func list
-index = -1
-length = len(list_4)
-# print('array\'s length - ', length)
-# print(list_4[index])
-
-counter = 0
-
-while counter < len(list_4):
-    print(list_4[counter])
-    counter += 1
-
-# slice depend sys sss
-# start(include), defalt - null
-# stop (uninc)
-# step, def - 1
-
-print(list_4[0:len(list_4):2])
-
-a = ['name', 'fio', 'salt', 'app', 'leaf', 'flame', 'fire']
-print(a[4:])
+def smart_greeting(hour):
+    if 12 < hour <= 7:
+        return 'Доброе утро!'
+    if 18 < hour <= 12:
+        return 'Добрый день!'
+    if 18 < hour <= 12:
+        return 'Добрый вечер!'
+    return 'Добрый ночи!'
