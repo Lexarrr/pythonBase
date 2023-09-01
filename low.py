4f6d748a873c03f1c4a16c0316569a991c7b86be
Lexarrr <etc.21@yandex.ru>
2023-09-01 20:52:23 +0300

    assertion

--- low.py
+++ low.py
@@ -1,15 +1,10 @@
 # moduls and their used
 # func antagonists
 # exception
-
-max_value = 10
-min_value = 1
+# Assertion
 
 try:
-    cur_val = int(input(f'input int from {min_value} to {max_value}: '))
-    if not min_value <= cur_val <= max_value:
-        raise ValueError('input int is not in diapason')
-except ValueError as exp:
-    print('You must to be attending:', exp)
-else:
-    print(f'yes. it\'s int exist in this diapason: {min_value}...{max_value}')
+    text = input('input any text: ')
+    assert len(text) > 3
+except AssertionError:
+    print('Length of text smallest then 3 symbols')
