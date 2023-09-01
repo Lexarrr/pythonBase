8cad71622fb731d4068ea6a068858f30946da15d
Lexarrr <etc.21@yandex.ru>
2023-09-01 19:41:30 +0300

    formatting for used f-string(since 3.6 >)

--- low.py
+++ low.py
@@ -1,11 +1,11 @@
 # moduls and their used
 # func antagonists
 # %s - line, %f - float', %d - int
-#
+# formatting for used f-string(since 3.6 >)
 
 name = 'Lexar'
 age = 21
 height = 171.66
 
-f_string = 'name: {:s}, \nage: {:d} year old, \nheight: {:.1f} sm.'.format(name, age, height)
+f_string = f'name: {name:20}, \nage: {age:3d} year old, \nheight: {height:3.1f} sm.'
 print(f_string)
