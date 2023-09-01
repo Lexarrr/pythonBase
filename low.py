02f7e3e29a159c731436e3ef6fa66e0928c64483
Lexarrr <etc.21@yandex.ru>
2023-09-01 19:04:09 +0300

    find

--- low.py
+++ low.py
@@ -1,6 +1,22 @@
-string = 'programs language pyton'
+# method find(substr, start = 0, end =none)
 
-print(string.lower())  # small letters
-print(string.upper())  # all big letters
-print(string.capitalize())  # first big letters
-print(string.title())  # all word with first big letters
+string = 'watching, listening, seeing'
+
+# if string.find('ing') != -1:
+if 'ing' in string:
+    count = string.count('ing')
+
+    print('"ing" to finds', count, 'times')
+
+    index = string.find('ing')  # from first symb(index) / start
+    print(index)
+
+    index = string.find('ing', index + 1)  # from any index
+    print(index)
+
+    length = len(string)
+
+    index = string.find('ing', index + 1, length)  # from any index
+    print(index)
+else:
+    print('not find')
