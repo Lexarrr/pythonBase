c2e62b27110a3add1d18ae0f46d4fbb6bed60712
Lexarrr <etc.21@yandex.ru>
2023-09-01 19:29:05 +0300

    positional formatting

--- low.py
+++ low.py
@@ -1,10 +1,11 @@
 # moduls and their used
 # func antagonists
 # %s - line, %f - float', %d - int
+#
 
 name = 'Lexar'
 age = 21
 height = 171.66
 
-f_string = 'name: %10s, \nage: %d year old, \nheight: %5.1f sm.' % (name, age, height)
+f_string = 'name: {0}, \nage: {1} year old, \nheight: {2} sm.'.format(name, age, height)
 print(f_string)
