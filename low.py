4bec10eb281958674abf1a6d3b2a376ec04aeff9
Lexarrr <etc.21@yandex.ru>
2023-09-01 20:47:17 +0300

    raising exp

--- low.py
+++ low.py
@@ -2,25 +2,14 @@
 # func antagonists
 # exception
 
-print('to count % on ten')
-
-num = input('input int: ')
+max_value = 10
+min_value = 1
 
 try:
-    value = int(num)  # try to do operation / to try string into int
-    res = 10 % value
-except ZeroDivisionError:
-    print(f'you can\'t - 10 / {num} ')  # to write doer except / division on zero
-    at_the_end = "no-no-no"
-except ValueError:
-    print('You have to input int')
-    print(f'and you input a: {num}')
-    at_the_end = "it's bad!"
-except Exception as e:
-    print('this is exp:', e.__class__.__name__)
-    at_the_end = "And that..."
+    cur_val = int(input(f'input int from {min_value} to {max_value}: '))
+    if not min_value <= cur_val <= max_value:
+        raise ValueError('input int is not in diapason')
+except ValueError as exp:
+    print('You must to be attending:', exp)
 else:
-    print(f'last of division 10 on {value} is {res}')  # if exp not be
-    at_the_end = "Thanks"
-finally:
-    print(at_the_end)  # to do all times
+    print(f'yes. it\'s int exist in this diapason: {min_value}...{max_value}')
